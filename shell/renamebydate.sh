6de86bb9a5543deaa6a8ef64bf776abcae651d15
Valentina Boichuk <bv@bamboogroup.eu>
2016-10-06 10:49:37 +0300

    18. updated renamebydate     (added short hach to name;     removed seconds;     extention to uppercase)

--- shell/renamebydate.sh
+++ shell/renamebydate.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
 for file in $(find *.JPG -type f); do
-    #echo "[${file}]..."
+    #echo "${file}"
 	filename=$(basename "$file")
     #echo "[${file}] = [${filename}]..."
 	filename="${filename%.*}"
 	extention=${file##*.}
+	EXTENTION=$(echo $extention | tr '[:lower:]' '[:upper:]' )
+	# echo "${extention} => ${EXTENTION}"
 
     #echo "[${file}] = [${filename}].[${extention}]..."
-	
+	#echo "${filename}"
+
+	hach=$(md5 $file | grep -oE "[0-9a-f]{32}")
+    hach=${hach:0:6}
+
 	# targetname=$(date -d @$(stat -f="%Y" "${file}") + "%Y.%m.%d_(%H-%M-%S)")
-	targetname=$(stat -f "%Sm" -t "%Y.%m.%d_(%H-%M-%S)" "${file}")
+	targetname=$(stat -f "%Sm" -t "%Y.%m.%d_(%H-%M)" "${file}")
+	targetname="$targetname-$hach"
 	# echo $targetname
 
     if [ "$filename" = "$targetname" ]
 	then
-		#echo "$file is OK"
 		continue
 	fi
 	
-	newname=$targetname
+   	newname=$targetname
 
 	i=0
-	while [ -f "$newname.$extention" ]
+	while [ -f "$newname.$EXTENTION" ]
 	do	
-		#echo "$newname exists"
-		i=$(( $i+1 ))		
+		i=$(( $i+1 ))
 		newname="$targetname-($i)"
 	done
-	
-	echo "${file} -> ${newname}.${extention}"
-	command="mv $file $newname.$extention"
+
+	echo "${file} -> ${newname}.${EXTENTION}"
+	command="mv $file $newname.$EXTENTION"
 	$command
-	
 done
