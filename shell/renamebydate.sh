e5120c5f45ef6bf9535038c10f9990db17b80253
Valentina Boichuk <bv@bamboogroup.eu>
2016-10-06 16:06:39 +0300

    19. updated renamebydate     (extracting date from old filename)

--- shell/renamebydate.sh
+++ shell/renamebydate.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-for file in $(find *.JPG -type f); do
+for file in $(find . -type f -iname '*.jpg' -maxdepth 1); do
+	if [ ! -f "${file}" ]; then
+		echo "erorr '${file}'"
+		continue
+	fi
     #echo "${file}"
-	filename=$(basename "$file")
-    #echo "[${file}] = [${filename}]..."
+	filename=$(basename "${file}")
+    # echo "[${file}] = [${filename}]"
 	filename="${filename%.*}"
+
 	extention=${file##*.}
 	EXTENTION=$(echo $extention | tr '[:lower:]' '[:upper:]' )
 	# echo "${extention} => ${EXTENTION}"
 
+	# try to find datetime in old file name
+	match=$(echo "${filename}" | grep -oE "[1-2][0-9][0-9][0-9][._-]?[0-1][0-9][._-]*[0-3][0-9]" | sed 's/[^0-9]//g' )
+	if [[ $match != '' ]]; then
+		# echo ">>> ${match}"
+		match=${match:0:4}.${match:4:2}.${match:6:2}
+		# echo ">>> ${match}"
+		targetname=$(stat -f "%Sm" -t "(%H-%M)" "${file}")
+		targetname="${match}_${targetname}"
+	else
+		# targetname=$(date -d @$(stat -f="%Y" "${file}") + "%Y.%m.%d_(%H-%M-%S)")
+		targetname=$(stat -f "%Sm" -t "%Y.%m.%d_(%H-%M)" "${file}")
+	fi
+
     #echo "[${file}] = [${filename}].[${extention}]..."
 	#echo "${filename}"
 
-	hach=$(md5 $file | grep -oE "[0-9a-f]{32}")
+	# Adding short hach to filename
+	hach=$(md5 "${file}" | grep -oE "[0-9a-f]{32}")
     hach=${hach:0:6}
-
-	# targetname=$(date -d @$(stat -f="%Y" "${file}") + "%Y.%m.%d_(%H-%M-%S)")
-	targetname=$(stat -f "%Sm" -t "%Y.%m.%d_(%H-%M)" "${file}")
 	targetname="$targetname-$hach"
+	
 	# echo $targetname
 
     if [ "$filename" = "$targetname" ]
